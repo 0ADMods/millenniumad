1984627b0ae1e750984d5aa07b1a85963725192d
niektb <niektb95@gmail.com>
2014-02-17 13:23:37 +0100

    [Sound] Second battle song commit
    Through Enemy Lines by Antii Martikainen

--- gui/common/music.js
+++ gui/common/music.js
@@ -53,7 +53,7 @@ Music.prototype.resetTracks = function()
 	this.tracks = {
 		MENU: ["To_the_Ends_of_the_Earth.ogg"],
 		PEACE: [],
-		BATTLE: ["Valor.ogg", "Taiko_2.ogg"],
+		BATTLE: ["Valor.ogg", "Through_Enemy_Lines.ogg"],
 		VICTORY : ["On_the_Road_to_Adventures.ogg"],
 		DEFEAT : ["Ominous_Revelations.ogg"]
 	};
