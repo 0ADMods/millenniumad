5c3c52c7bbd44cb34d592aee32ab94ec163e25d8
s0600204 <dev@s0600204>
2015-06-24 15:39:10 +0100

    Adapt main-menu background  artwork to use sanderd17's new framework

---
+++ gui/pregame/backgrounds/black_forest.js
@@ -0,0 +1,8 @@
+g_BackgroundLayerData.push(
+	[
+		{
+			"offset": (time, width) => 0.12 * width * Math.cos(0.05 * time)+768,
+			"sprite": "background-black_forest1-1",
+			"tiling": true,
+		},
+	]);
