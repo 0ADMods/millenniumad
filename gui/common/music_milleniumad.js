ca341198b142f3df8d581adeb278a1e0745e7c0b
mrlie58 <60375368+mrlie58@users.noreply.github.com>
2020-07-12 23:27:12 +0200

    Add music from Main-game and fix byza_mechanical_siege_traction ...

---
+++ gui/common/music_milleniumad.js
@@ -0,0 +1,15 @@
+Music.prototype.resetTracks = function()
+{
+	this.tracks = {
+		"MENU": ["To_the_Ends_of_the_Earth.ogg"],
+		"PEACE": [
+			"Tale_of_Warriors.ogg",
+			"Tavern_in_the_Mist.ogg",
+			"The_Road_Ahead.ogg"
+		],
+		"BATTLE": ["Valor.ogg", "Through_Enemy_Lines.ogg"],
+		"VICTORY": ["On_the_Road_to_Adventures.ogg"],
+		"DEFEAT": ["Ominous_Revelations.ogg"],
+		"CUSTOM": []
+	};
+};
