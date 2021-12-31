ddb2c6b4b1a522c5968102eb88d74ebe10e18601
stereotipo <93648099+stereotipo@users.noreply.github.com>
2021-12-31 22:21:33 +0100

    Add files via upload

--- gui/pregame/backgrounds/desert.js
+++ gui/pregame/backgrounds/desert.js
@@ -1,18 +1,18 @@
 g_BackgroundLayerData.push(
 	[
 		{
-			"offset": (time, width) => 0.07 * width * Math.cos(0.1 * time),
-			"sprite": "desert-03",
-			"tiling": true
-		},
-		{
-			"offset": (time, width) => 0.05 * width * Math.cos(0.1 * time),
+			"offset": (time, width) => 0.00 * width * Math.cos(0.0 * time),
 			"sprite": "desert-01",
-			"tiling": true
+			"tiling": false
 		},
 		{
-			"offset": (time, width) => 0.05 * width * Math.cos(0.1 * time),
+			"offset": (time, width) => 0.00 * width * Math.cos(0.0 * time),
 			"sprite": "desert-02",
-			"tiling": true
+			"tiling": false
+		},
+		{
+			"offset": (time, width) => 0.16 * width * Math.cos(0.08 * time),
+			"sprite": "desert-03",
+			"tiling": false
 		}
 	]);
